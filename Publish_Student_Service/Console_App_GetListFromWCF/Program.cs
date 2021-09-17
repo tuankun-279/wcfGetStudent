b31f702e25bf1a530e263b7bc9c119b061da715d
tuankun-279 <59610132+tuankun-279@users.noreply.github.com>
2021-09-17 09:48:42 -0700

    nice

---
+++ Publish_Student_Service/Console_App_GetListFromWCF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_GetListFromWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReferenceGetListStudent.Service1Client myService = new ServiceReferenceGetListStudent.Service1Client();
+            var lists = myService.Get_ListSinhVien();
+            foreach( var m in lists)
+            {
+                Console.WriteLine("ID: {0} | Name : {1} ",m.Id,m.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Publish_Student_Service/Publish_Student_Service/SinhVien.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Publish_Student_Service
+{
+    [DataContract]
+    public class SinhVien
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        //[DataMember]
+        public string Email { get; set; }
+    }
+
+    public class TestSinhVien
+    {
+        public static List<SinhVien> GetSinhVien()
+        {
+            List<SinhVien> sinhViens = new List<SinhVien>();
+            //sinhViens.Add(new SinhVien() { Id = 1, Name = "Tuan", Email = "tuan@gmail.com" });
+            //sinhViens.Add(new SinhVien() { Id = 2, Name = "Linh", Email = "linh@gmail.com" });
+            //sinhViens.Add(new SinhVien() { Id = 3, Name = "Minh", Email = "minh@gmail.com" });
+            //sinhViens.Add(new SinhVien() { Id = 4, Name = "Duy", Email = "duy@gmail.com" });
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-03RSBTH;Initial Catalog=GetStudent;Integrated Security=True");
+            string sql = "select * from SinhVien ";
+            con.Open();
+            SqlCommand com = new SqlCommand(sql, con);
+            
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                sinhViens.Add(new SinhVien()
+                {
+                    Id = Convert.ToInt32(row["Id"].ToString()),
+                    Name = row["Name"].ToString(),
+                    Email = row["Email"].ToString()
+                });
+            }
+            return sinhViens;
+        }
+    }
+}
